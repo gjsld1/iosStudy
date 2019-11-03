6a62b23d65d6511732b66f31d3305489a07a9b8b
신예지 <gjsld1@naver.com>
2019-11-03 15:30:52 +0900

    first commit

---
+++ HowlTalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HowlTalk
+//
+//  Created by 신예지 on 03/11/2019.
+//  Copyright © 2019 신예지. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
