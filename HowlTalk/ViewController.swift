06c9bd0988baef188ee36feb01025ac1d48742ef
신예지 <gjsld1@naver.com>
2019-11-03 16:48:03 +0900

    make simple view

--- HowlTalk/ViewController.swift
+++ HowlTalk/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var mainImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textLabel.text = "Testing input.."
+       
+        actionButton.addTarget(self, action: #selector(actionEvent), for: UIControl.Event.touchUpInside)
+        //self는 자기 클래스 지칭, for는 어떤 상태일 때 실행할지
+        
     }
-
-
+    @objc func actionEvent() {
+        textLabel.text = textField.text
+        mainImageView.image = UIImage(named:"mugadang")
+    }
 }
 
